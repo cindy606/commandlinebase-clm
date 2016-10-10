e70aaf5db3fec809d8ac8f09471e9e200ee08b4b
cindy606 <cindy@Cindys-MacBook-Pro.local>
2016-10-10 11:42:51 -0400

    Added base classes

---
+++ CommandLineBase-clm/CommandLineBase-clm/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  CommandLineBase-clm
+//
+//  Created by Cindy on 10/10/16.
+//  Copyright © 2016 Interapt. All rights reserved.
+//
+
+import Foundation
+
+let interactive = Interactive ()
+
+// Run in interactive mode
+interactive.go()
+
+exit(EXIT_SUCCESS)
+
+
